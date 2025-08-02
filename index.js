e8ece4c8b4be774c085e7ba276f214c5ab75e9f2
unknown <arumansalik45@gmail.com>
2025-08-02 10:44:06 +0530

    Add Skeleton of Routes

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 const express = require('express'); 
+const app = express();
+
+app.post("/user/signup", function(req, res) {
+    res.json({
+        message: "signup endpoint"
+    })
+})
+
+app.post("/user/signin", function(req, res) {
+    res.json({
+        message: "signup endpoint"
+    })
+})
+
+app.post("/user/purchases", function(req, res) {
+    res.json({
+        message: "signup endpoint"
+    })
+})
+
+app.post("/courses/purchase", function(req, res) {
+    res.json({
+        message: "signup endpoint"
+    })
+})
+
+app.post("/courses", function(req, res) {
+    res.json({
+        message: "signup endpoint"
+    })
+})
